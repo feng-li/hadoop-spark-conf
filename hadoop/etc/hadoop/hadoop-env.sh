d0b2dd3be63cf0157c93fc1cb22b83744e40a9df
Feng Li <m@feng.li>
2022-09-02 10:47:26 +0800

    Update config for Spark3.3-hadoop3.3.4-scala2.13

--- hadoop/etc/hadoop/hadoop-env.sh
+++ hadoop/etc/hadoop/hadoop-env.sh
@@ -144,7 +144,7 @@ export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
 # Enable optional, bundled Hadoop features
 # This is a comma delimited list.  It may NOT be overridden via .hadooprc
 # Entries may be added/removed as needed.
-# export HADOOP_OPTIONAL_TOOLS="hadoop-azure,hadoop-aliyun,hadoop-aws,hadoop-openstack,hadoop-kafka,hadoop-azure-datalake"
+# export HADOOP_OPTIONAL_TOOLS="hadoop-kafka,hadoop-openstack,hadoop-aliyun,hadoop-azure,hadoop-azure-datalake,hadoop-aws"
 
 ###
 # Options for remote shell connectivity
@@ -415,3 +415,16 @@ export HADOOP_LOG_DIR=/home/hadoop/logs
 #
 # For example, to limit who can execute the namenode command,
 # export HDFS_NAMENODE_USER=hdfs
+
+
+###
+# Registry DNS specific parameters
+###
+# For privileged registry DNS, user to run as after dropping privileges
+# This will replace the hadoop.id.str Java property in secure mode.
+# export HADOOP_REGISTRYDNS_SECURE_USER=yarn
+
+# Supplemental options for privileged registry DNS
+# By default, Hadoop uses jsvc which needs to know to launch a
+# server jvm.
+# export HADOOP_REGISTRYDNS_SECURE_EXTRA_OPTS="-jvm server"
